f53444977d28ee395bf5ba2bed2df5e91f108f5f
Meskedal <marius.eskedal@gmail.com>
2018-01-06 14:15:13 +0100

    Site hosting page with arguemnts

--- html_creator.py
+++ html_creator.py
@@ -3,17 +3,19 @@
 import sys, os
 
 def main():
-	file_path = 'C:/Users/Eskedal/Desktop'
-	complete_name = os.path.join(file_path, "arguments.html")
+	file_path = '/var/www/html/wp-content/themes/twentyseventeen'
+	complete_name = os.path.join(file_path, "arguments.php")
 	f = open(complete_name,'w+')
 	argument_list = sys.argv[1:len(sys.argv)]
 	arguments = ""
 	for i in range(0, len(argument_list)):
 		arguments += argument_list[i]
 		if(i != len(argument_list)-1):
-			arguments += ' '
-	header = "<html> <head><p>"+arguments+"</p></head></html>"
-
+			arguments+= ' '
+	template = "<?php /* Template Name: Custom-Page */ ?>"
+	#header = "\n<html> <head><p>"+arguments+"</p></head></html>"
+	header = arguments
+	f.write(template)
 	f.write(header)
 	f.close()
 
